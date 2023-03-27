d076cc9dbda7b8d20664b711ffa7431655425a16
João Vitor <joaovitorcfdev@gmail.com>
2023-03-27 11:46:35 -0300

    criando projeto

---
+++ src/App.java
@@ -0,0 +1,29 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endress = URI.create(url);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endress).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> movieList = parser.parse(body);
+
+        for (Map<String, String> movie : movieList) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
